1cc846e1d3d4b6f52be357fde01d509f4032e64a
Валерий <st091361@student.spbu.ru>
2021-12-08 21:42:37 +0300

    второй коммит

--- src/fewfwef.java
+++ src/fewfwef.java
@@ -1,6 +1,8 @@
 public class Task {
     public static void main (string[]args) {
         sout(sum(2,3));
-        int z=sum(2,3);
+        int z = (sum(2,3)
     }
-}
+};
+//новый комментарий
+//Я открыл репозиторий на гитхаб!
